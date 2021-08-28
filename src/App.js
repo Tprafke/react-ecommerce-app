4bc1f5a644235b18251ad9d8ee3cb27975b50e6d
toby <rtprafke96@gmail.com>
2021-08-28 15:40:37 -0400

    navbar

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import { Switch, Route } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
+import NavBar from "./components/navbar/navbar.component";
 
 function App() {
   return (
     <div>
+      <NavBar />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
