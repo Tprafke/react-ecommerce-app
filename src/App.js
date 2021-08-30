9770964a75763ee3814c53e100d684ebd5ab46e7
toby <rtprafke96@gmail.com>
2021-08-30 16:23:56 -0400

    sign-in-form

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import NavBar from "./components/navbar/navbar.component";
+import SignInAndRegister from "./pages/sign-in-and-register/sign-in-and-register.component";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInAndRegister} />
       </Switch>
     </div>
   );
---
+++ src/pages/sign-in-and-register/sign-in-and-register.component.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import SignIn from "../../components/sign-in/sign-in.component";
+
+import "./sign-in-and-register.styles.scss";
+
+const SignInAndRegister = () => (
+  <div className='sign-in-and-register'>
+    <SignIn />
+  </div>
+);
+
+export default SignInAndRegister;
