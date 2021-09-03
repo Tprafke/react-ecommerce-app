a472c228e9d0a3a5216a51e02be521dea877e9af
toby <rtprafke96@gmail.com>
2021-09-03 19:54:53 -0400

    auth-state-and-sign-in-styling

--- src/App.js
+++ src/App.js
@@ -4,18 +4,44 @@ import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import NavBar from "./components/navbar/navbar.component";
 import SignInAndRegister from "./pages/sign-in-and-register/sign-in-and-register.component";
+import { auth } from "./firebase/firebase.utils";
+import React from "react";
 
-function App() {
-  return (
-    <div>
-      <NavBar />
-      <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route path='/shop' component={ShopPage} />
-        <Route path='/signin' component={SignInAndRegister} />
-      </Switch>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      currentUser: null,
+    };
+  }
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
+      this.setState({ currentUser: user });
+
+      console.log(user);
+    });
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
+  render() {
+    return (
+      <div>
+        <NavBar currentUser={this.state.currentUser} />
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/shop' component={ShopPage} />
+          <Route path='/signin' component={SignInAndRegister} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
