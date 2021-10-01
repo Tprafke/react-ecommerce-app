2b932d0b999bfa458238ac0d60d95a5d278c2eb7
toby <rtprafke96@gmail.com>
2021-10-01 16:22:15 -0400

    redux-persist-cart-state

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,15 +1,19 @@
 import { createStore, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
-
 import logger from "redux-logger";
+import { persistStore } from "redux-persist";
 
 import rootReducer from "./root-reducer";
 
 const middlewares = [logger];
 
-const store = createStore(
+export const store = createStore(
   rootReducer,
   composeWithDevTools(applyMiddleware(...middlewares))
 );
 
-export default store;
+export const persistor = persistStore(store);
+
+const persistedStore = { store, persistor };
+
+export default persistedStore;
