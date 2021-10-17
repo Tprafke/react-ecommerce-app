fde09a919bbab30baeeea529afd0a0b993892671
toby <rtprafke96@gmail.com>
2021-10-17 16:38:07 -0400

    styling-responsive-checkout-and-sign-in-pages

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -5,7 +5,11 @@ import { signInWithEmail, socialLogin } from "../../firebase/firebaseService";
 
 import FormInput from "../form-input/form-input.component";
 
-import "./sign-in.styles.scss";
+import {
+  SignInContainer,
+  SignInTitle,
+  ButtonsBarContainer,
+} from "./sign-in.styles";
 
 class SignIn extends React.Component {
   constructor(props) {
@@ -33,8 +37,8 @@ class SignIn extends React.Component {
 
   render() {
     return (
-      <div className='sign-in'>
-        <h2>I already have an account</h2>
+      <SignInContainer>
+        <SignInTitle>I already have an account</SignInTitle>
         <span>Sign in with your email and password</span>
 
         <form onSubmit={this.handleSubmit}>
@@ -55,15 +59,15 @@ class SignIn extends React.Component {
             label='Password'
             required
           />
-          <div className='buttons'>
+          <ButtonsBarContainer>
             <CustomButton type='submit'>Sign in</CustomButton>
             <CustomButton onClick={() => socialLogin("google")} isGoogleSignIn>
               {" "}
               Sign in with Google{" "}
             </CustomButton>
-          </div>
+          </ButtonsBarContainer>
         </form>
-      </div>
+      </SignInContainer>
     );
   }
 }
